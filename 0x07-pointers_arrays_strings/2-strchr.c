3b445af423089148b83e2c8d18278ac64748d1f4
sephiealx <koutanyi@gmail.com>
2022-09-26 18:34:38 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,16 +16,16 @@ char *_strchr(char *s, char c)
 				if (*s == c) /*if s ==  c*/
 					{
 						return (s); /*return s*/
-																}
+					}
 
 									++s;
-										}
+				}
 
 			if (*s == c)
 					{
 								return (s);
 
-									}
+					}
 
 
 				return (0);/* values null*/

1910196dc7a2e97e0b6ebbf74619f0833c3414a9
sephiealx <koutanyi@gmail.com>
2022-09-26 18:05:11 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,26 +6,19 @@
  * @c: Character to search
  * Return: Pointer to the first occurrence of the character c in the string s
  */
-
 char *_strchr(char *s, char c)
 {
-
-		while (*s != '\0'); /*Declaring WHILE*/
-			{
-				if (*s == c) /*if s ==  c*/
-				{
-				return (s); /*return s*/
-				}
-
-				++s;
-			}
-
-			if (*s == c)
-			{
-				return (s);
-
-			}
-
-
-			return (0);/* values null*/
+	int a;
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
 }
